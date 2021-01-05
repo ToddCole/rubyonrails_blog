e63e37f75c56c3f13dcd12758b3c05bab71abae4
ToddCole <todd@yogajournal.com.au>
2021-01-05 17:47:50 +1000

    added route article index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get "/articles", to: "articles#index"
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
+Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
