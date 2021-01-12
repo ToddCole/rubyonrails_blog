d53cc9f8779ce8a1074395c5ad0ac2650bde6c75
ToddCole <todd@yogajournal.com.au>
2021-01-13 09:02:15 +1000

    new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,5 +6,18 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+end
